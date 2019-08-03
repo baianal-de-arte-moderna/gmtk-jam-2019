aa589ac7e2ebaaeaa7d02288148e29958823adeb
Piramba <avpaganini@gmail.com>
2019-08-03 15:58:18 -0300

    WebGL Build, character and input system

---
+++ OnlyOne/Assets/Input/Controls.cs
@@ -0,0 +1,182 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/Controls.inputactions'
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class Controls : IInputActionCollection
+{
+    private InputActionAsset asset;
+    public Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""0bda9965-42bf-4070-a3ac-61c86cfbd4b7"",
+            ""actions"": [
+                {
+                    ""name"": ""Walk"",
+                    ""type"": ""Button"",
+                    ""id"": ""65e568a8-e1ec-4b48-b1a8-544ace1522d3"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""57058251-484f-45b6-ba3c-0ee7b26a0f9a"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""73cf55a2-f6d6-451d-a6e5-eca4c9258791"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""593743b7-30d6-4899-8fce-d4c5960b5e35"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""2c141424-0e3e-4997-b06d-4f3ac4e26f50"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5c2dfc01-e1f6-41ef-b43a-e02703089fca"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.GetActionMap("Player");
+        m_Player_Walk = m_Player.GetAction("Walk");
+        m_Player_Jump = m_Player.GetAction("Jump");
+    }
+
+    ~Controls()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Walk;
+    private readonly InputAction m_Player_Jump;
+    public struct PlayerActions
+    {
+        private Controls m_Wrapper;
+        public PlayerActions(Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Walk => m_Wrapper.m_Player_Walk;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                Walk.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnWalk;
+                Walk.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnWalk;
+                Walk.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnWalk;
+                Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Walk.started += instance.OnWalk;
+                Walk.performed += instance.OnWalk;
+                Walk.canceled += instance.OnWalk;
+                Jump.started += instance.OnJump;
+                Jump.performed += instance.OnJump;
+                Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnWalk(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ OnlyOne/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Space(10)]
+    [Header("Environment")]
+    public float gravity;
+
+    [Space(10)]
+    [Header("Movement")]
+    public float topSpeed;
+    public int tractionFrames;
+    public AnimationCurve traction;
+    public int slowdownFrames;
+    public AnimationCurve slowdown;
+
+
+    new Rigidbody rigidbody;
+    Controls controls;
+    Vector3 move;
+    Vector3 nextVelocity;
+    int frameCount;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controls = new Controls();
+        rigidbody = GetComponent<Rigidbody>();
+        move = Vector3.zero;
+
+        controls.Player.Walk.started += ctx => Move(ctx.ReadValue<float>());
+        controls.Player.Walk.canceled += ctx => Move(0);
+    }
+
+    void onEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    void onDisable()
+    {
+        controls.Player.Disable();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gravity
+        rigidbody.velocity += Vector3.down * gravity * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        // Move
+        if (move.x != 0)
+        {
+            frameCount = Mathf.Min(frameCount + 1, tractionFrames);
+            nextVelocity = rigidbody.velocity;
+            nextVelocity.x = topSpeed * traction.Evaluate(frameCount / tractionFrames) * move.x;
+            rigidbody.velocity = nextVelocity;
+        }
+        else if (rigidbody.velocity.x != 0f)
+        {
+            frameCount = Mathf.Min(frameCount + 1, tractionFrames);
+            nextVelocity = rigidbody.velocity;
+            nextVelocity.x = topSpeed * traction.Evaluate(frameCount / slowdownFrames);
+            rigidbody.velocity = nextVelocity;
+        }
+    }
+
+    void Move(float multiplier)
+    {
+        if ((move.x == 0 && multiplier != 0) ||
+            (move.x != 0 && multiplier == 0))
+        {
+            // Moving State changed
+            frameCount = 0;
+        }
+        move.x = multiplier;
+        Debug.Log(move.x);
+    }    
+}
