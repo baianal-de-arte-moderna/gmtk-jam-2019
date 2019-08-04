6c15b90b92475c18ca983e3feeb47bdd422c45fe
Vinicius Uzeda <vruzeda@gmail.com>
2019-08-04 12:05:19 -0300

    Enemy spawner

---
+++ OnlyOne/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    private GameObject enemyInstance;
+
+    private void Start() {
+        Spawn();
+    }
+
+    public void Spawn() {
+        if (!enemyInstance) {
+            enemyInstance = Instantiate(enemyPrefab, transform);
+        }
+    }
+}
