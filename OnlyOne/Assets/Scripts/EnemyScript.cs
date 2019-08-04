0cc1176d8a3b92e0f215224142755dba4b67c97e
Marco <dkeeeper@gmail.com>
2019-08-04 05:12:38 -0300

    Add enemy scene

---
+++ OnlyOne/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class PlayerHitEvent : UnityEvent {
+}
+
+public class EnemyScript : MonoBehaviour
+{
+    public Collider drawingInstanceCollider;
+    public Collider attackingCollider;
+    public Collider hurtboxCollider;
+    public GameObject enemyEntity;
+
+    [SerializeField]
+    private PlayerHitEvent OnStartPlayerHitEvent;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == drawingInstanceCollider)
+        {
+            //trigger  weapon drawing animation
+        }
+
+        if (other == attackingCollider)
+        {
+            OnStartPlayerHitEvent?.Invoke();
+        }
+
+        if (other == hurtboxCollider)
+        {
+            Destroy(enemyEntity);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //trigger sheathing animation
+    }
+}
