11e2aed9f3f6f0863de7ffc10a1192092034ea8a
Vinicius Uzeda <vruzeda@gmail.com>
2019-08-04 00:36:30 -0300

    Initial respawn implementation

---
+++ OnlyOne/Assets/Scripts/Respawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour {
+
+    private List<Respawnable> respawnables;
+    private int currentCheckpointInterval;
+
+    private void Start() {
+        respawnables = new List<Respawnable>(FindObjectsOfType<Respawnable>());
+    }
+
+    public void Respawn() {
+
+    }
+}
