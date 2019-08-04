2cb355704b390491b2090947961ca1dc6bdae971
Aléxis Kiosia <alexis.kiosia@gmail.com>
2019-08-04 15:12:08 -0300

    Add menus

---
+++ OnlyOne/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void OnCreditsBackButtonClicked()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene("CharacterAndEnemy");
+    }
+
+    public void OnCreditsButtonClicked()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
